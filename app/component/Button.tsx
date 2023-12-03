84968dc0b751a77aa1688a7106587c7e6b59aca1
eddiebee <edbeeotudor@gmail.com>
2023-12-03 12:12:29 +0100

    update Button

--- app/component/Button.tsx
+++ app/component/Button.tsx
@@ -1,20 +1,43 @@
 import { Link } from "@remix-run/react";
+import { ReactNode } from "react";
 
-const Button = ({ text }: { text: string }) => {
+const Button = ({
+  text,
+  onClick,
+  children,
+}: {
+  text: string;
+  onClick?: () => void;
+  children?: ReactNode;
+}) => {
   return (
-    <button className="text-white text-md bg-yellow rounded-md p-2 w-[11rem] tracking-wide font-normal hover:bg-transparent hover:border-yellow hover:border-2 hover:transition-all">
+    <button
+      className="flex justify-center text-white text-md bg-yellow rounded-md p-2 w-[11rem] tracking-wide font-normal hover:bg-transparent hover:border-yellow hover:border-2 hover:transition-all"
+      onClick={onClick}
+    >
       {text}
+      {children}
     </button>
   );
 };
 
-const LinkButton = ({ to, text }: { to?: string; text: string }) => {
+const LinkButton = ({
+  to,
+  text,
+  onClick,
+  children,
+}: {
+  to?: string;
+  text: string;
+  onClick?: () => void;
+  children?: ReactNode;
+}) => {
   return to ? (
     <Link to={to}>
-      <Button text={text} />
+      <Button text={text} onClick={onClick} children={children} />
     </Link>
   ) : (
-    <Button text={text} />
+    <Button text={text} onClick={onClick} children={children} />
   );
 };
 
