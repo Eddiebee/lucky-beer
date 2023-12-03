ae48282de52c27d46810839cbdeb45f70ea0803a
eddiebee <edbeeotudor@gmail.com>
2023-12-03 12:12:17 +0100

    update BackButton

--- app/component/BackButton.tsx
+++ app/component/BackButton.tsx
@@ -6,7 +6,7 @@ const BackButton = () => {
 
   return (
     <ChevronLeftIcon
-      className="absolute top-28 w-12 mx-7  md:mx-20 text-yellow hover:text-white hover:transition-all cursor-pointer"
+      className="absolute top-28 w-8 mx-7  md:mx-20 text-yellow hover:text-white hover:transition-all cursor-pointer"
       onClick={() => navigate(-1)}
     />
   );
