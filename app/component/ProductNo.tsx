dc95c7ab3bb70d3facfdc1bb7cf4c615c850d374
eddiebee <edbeeotudor@gmail.com>
2023-12-03 12:12:05 +0100

    build out productNo section

--- app/component/ProductNo.tsx
+++ app/component/ProductNo.tsx
@@ -1,17 +1,36 @@
-import { Form } from "@remix-run/react";
+import {  useNavigate } from "@remix-run/react";
 import Button from "./Button";
+import { useState } from "react";
 
 const ProductNo = () => {
+  // validate that the input corresponds to ABC123
+  const [productNo, setProductNo] = useState("");
+  const [isError, setIsError] = useState(false);
+  const navigate = useNavigate();
+
+  const validateProductNo = (productNo: string) => {
+    if (productNo.toLowerCase() === "abc123") {
+      navigate("/result");
+    } else {
+      setIsError(true);
+    }
+  };
+
   return (
     <div className="flex flex-col text-white mt-10 gap-8">
       <h1 className="text-3xl">Input Product No</h1>
       <p className="w-[25rem] text-xl font-light ">
-        Input the 6 digits product no located under the cover of lucky beer
+        Input the 6 digit product number located on the body of Lucky Beer
       </p>
-      <Form>
-        <div>
-          <input
-            required
+      <div>
+        {isError && (
+          <p className="text-yellow text-lg p-5 ">Invalid product number</p>
+        )}
+        <div className="flex flex-col items-center self-center">
+          <div>
+            <input
+              value={productNo}
+              onChange={(e) => setProductNo(e.target.value)}
               required
               placeholder="xxxxxxx"
               type="text"
@@ -20,15 +39,11 @@ const ProductNo = () => {
             />
           </div>
           <div>
+            <Button text="SCAN" onClick={() => validateProductNo(productNo)} />
+          </div>
         </div>
-      </Form>
+      </div>
     </div>
   );
-              required
-              placeholder="xxxxxxx"
-              type="text"
-              inputMode="numeric"
-              className="p-1 w-[20rem] text-center outline-none tracking-wider bg-transparent border-yellow border-2 rounded-md mb-8"
-            />
-          </div>
-          <div>
+};
+export default ProductNo;
