8d6d112f2abdcf196b130882f17f64b5a0b56298
eddiebee <edbeeotudor@gmail.com>
2023-12-03 12:11:01 +0100

    add states for values

--- app/component/ProductNo.tsx
+++ app/component/ProductNo.tsx
@@ -2,6 +2,7 @@ import { Form } from "@remix-run/react";
 import Button from "./Button";
 
 const ProductNo = () => {
+  return (
   // validate that the input corresponds to ABC123
   const [productNo, setProductNo] = useState("");
   const [isError, setIsError] = useState(false);
@@ -15,7 +16,6 @@ const ProductNo = () => {
     }
   };
 
-  return (
     <div className="flex flex-col text-white mt-10 gap-8">
       <h1 className="text-3xl">Input Product No</h1>
       <p className="w-[25rem] text-xl font-light ">
@@ -37,5 +37,11 @@ const ProductNo = () => {
       </Form>
     </div>
   );
-};
-export default ProductNo;
+              required
+              placeholder="xxxxxxx"
+              type="text"
+              inputMode="numeric"
+              className="p-1 w-[20rem] text-center outline-none tracking-wider bg-transparent border-yellow border-2 rounded-md mb-8"
+            />
+          </div>
+          <div>
