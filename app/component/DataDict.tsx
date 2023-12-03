1a5bd1269f93adbba12629e1936cab1ba70aa9d2
eddiebee <edbeeotudor@gmail.com>
2023-12-03 12:12:50 +0100

    format text for capitalize

--- app/component/DataDict.tsx
+++ app/component/DataDict.tsx
@@ -1,7 +1,7 @@
 const DataDict = ({ dt, dd }: { dt: string; dd: string }) => {
   return (
     <div className="flex justify-between gap-3 text-base mb-2 text-left font-light">
-      <dt>{dt}</dt>
+      <dt>{dt.charAt(0).toUpperCase() + dt.substring(1)}</dt>
       <dd className="w-[14rem]">{dd}</dd>
     </div>
   );
